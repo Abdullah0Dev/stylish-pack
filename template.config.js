d6309dca563719a8af5c798bb649d8f83100475e
Abdullah - Web Minds <160670132+Abdullah0Dev@users.noreply.github.com>
2025-02-15 08:15:36 +0200

    first commit

--- template.config.js
+++ template.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   placeholderName: "MyAwesomeApp", // This is the name that will be replaced in the template files
   templateDir: "./template",
-  postInitScript: "ts-node scripts/post-init.ts", // No need for {{name}}, will be handled in the script
+  titlePlaceholder: 'Hello App Display Name',
+  // postInitScript: "node ./scripts/post-init.js", // No need for {{name}}, will be handled in the script
 };
